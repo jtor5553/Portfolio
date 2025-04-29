efe20ce893266f8c899713ed943d1054bb75caad
Jose Torres-Gomez <143902932+jtor5553@users.noreply.github.com>
2025-04-29 15:12:16 -0500

    Add files via upload

---
+++ MealTime/MealTime/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace MealTime;
+
+
+
+2class Program
+{
+    static void Main(string[] args)
+    {
+
+        /*Prompt the user to enter the time.*/
+Console.WriteLine("Enter the time of day\n");
+
+        /*Get the user response.*/
+string time = Console.ReadLine();
+
+        /*Send the user response to the convertTime  function to convert the time to a floating point value.*/
+float timeInFloat = convertTime(string time);
+        /*Use the converted time to determine whether it is  Breakfast time, Lunch time, or Dinner time. 
+        Breakfast between 7:00 and 8:00, Lunch between 12:00 and 13:00,  Dinner between 18:00 and 19:00
+        Output the appropriate message to the user.
+        End program*/
+float breakfastStart, breakfastEnd, lunchStart, lunchEnd, dinnerStart, dinnerEnd;
+    breakfastStart = 7.0;
+    breakfastEnd = 8.0;
+    lunchStart = 12.0;
+    lunchEnd = 13.0;
+    dinnerStart = 18.0;
+    dinnerEnd = 19.0;
+
+        /* 
+        The meal times are breakfast between 7:00 and 8:00, lunch between 12:00 and 13:00, and dinner between 
+        18:00 and 19:00. Assume that the user’s input will be formatted in 24-hour time as #:## or ##:##. And 
+        assume that each meal’s time range is inclusive of the end points in the time range. For instance, whether 
+        it’s 7:00, 7:01, 7:59, or 8:00, or anytime in between, it’s time for breakfast.*/
+if(timeInFloat >= breakfastStart && timeInFloat <= breakfastEnd){
+    Console.WriteLine("Its breakfast time!!!");
+}else if(timeInFloat >= lunchStart && timeInFloat <= lunchEnd){
+    Console.WriteLine("Lunch time!!!");
+}else if(timeInFloat >= dinnerStart && timeInFloat <= dinnerEnd){
+    Console.WriteLine("Dinner time!!!");
+}else{Console.WriteLine("Not time to eat!!!");}
+
+    }
+
+     /*Your are to also create a function called convertTime  (that can be called by main) that 
+        accepts one parameter, the string time the user entered, and converts time, a string in 
+        24-hour format, to the corresponding number of hours as a float. For instance, given a 
+        time like "7:30" (i.e., 7 hours and 30 minutes), convert should return 7.5 (i.e., 7.5 hours).*/
+
+    public static float convertTime(time){
+    int hours, minutes;
+
+     string[] twoParts = time.Split(':'); 
+
+        hours = int.Parse(twoParts[0]);
+        minutes = int.Parse(twoParts[1]);
+
+        return (hours + minutes) / 60.0;
+    }
+}
+
+
---
+++ OOPInheritance/OOPInheritance/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Reflection;
+using OOPInheritance;
+
+//I ran out of time sorry,
+
+namespace OOPInheritance;
+using System;
+using System.Reflection.Metadata;
+
+enum EmployeeType{Sales, Manager, Production}
+enum SalesLevel({Platinum, Diamomd, GCCollectionMode, Silver, Bronze}
+
+class Employee{
+    private string firstName, lastName, id;
+    private EmployeeType empType;
+
+    public Employee(string firstName, string lastName, string id, EmployeeType empType){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id =  id;
+        this.empType = empType;
+    }
+    public string GetFirstName(){return firstName;}
+    public void SetFirstName(string firstName)(this.firstName = firstName;)
+
+    public string GetLastName(){return lastName;}
+    public void SetLastName(string lastName){this.lastName = lastName;}
+
+    public string GetId(){return id;}
+    
+    public EmployeeType GetEmpType(){return empType;}
+    public void SetEmpType(EmployeeType empType){this.empType = empType;}
+
+public void GetEmployeeInfo(){
+    Console.WriteLine($"Name: {firstName} {lastName}\nID: {id}\nType: {empType}");
+}
+}
+
+class SalesPerson:Employee{
+    private string department;
+    private float sales;
+
+    public SalesPerson(string firstName, string lastName, string id, string department,float sales):base(firstName, lastName,id, EmployeeType.Sales){
+        this.department = department;
+        this.sales = sales;
+    }
+}
+
+public string GetDepartment(){return department;}
+public void SetDepartment(string department){ThreadStaticAttribute.department = department;}
+
+public float GetSales(){return sales;}
+
+public void UpdateSales(float additionalSales){sales = sales + additionalSales}
+
+public SalesLevel GetSalesLevel(){
+
+    if(sales >= 40000){return SalesLevel.Platinum;}
+    if(sales >= 30000){return SalesLevel.Diamond;}
+    if(sales >= 20000){return SalesLevel.Gold;}
+    if(sales >= 10000){return SalesLevel.Silver;}
+
+        return SalesLevel.Bronze;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
